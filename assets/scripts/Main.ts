75914f10ba0f7631cb3c334b4fe062412a446c49
Manuel Nascimento <57446204+Manuel-Antunes@users.noreply.github.com>
2021-05-19 14:02:52 -0300

    Update Main.ts

--- assets/scripts/Main.ts
+++ assets/scripts/Main.ts
@@ -1,10 +1,3 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -21,9 +14,4 @@ export default class Main extends cc.Component {
         this.collisionManager.enabled = true;
     }
 
-    start () {
-
-    }
-
-    // update (dt) {}
 }
