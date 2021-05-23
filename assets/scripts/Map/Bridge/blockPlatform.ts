50452e96c744f7cb29cf1cd646255c6cf8f1de66
LOFyz <leonardo_farias2010@hotmail.com>
2021-05-22 21:55:23 -0300

    Chore: I maked the animation play and pause if the actor is in contact or not.

--- assets/scripts/Map/Bridge/blockPlatform.ts
+++ assets/scripts/Map/Bridge/blockPlatform.ts
@@ -10,13 +10,17 @@ export default class blockPlatform extends cc.Component {
 
   public onBeginContact(_contact: Event, _selfCollider: cc.BoxCollider, _otherCollider: cc.BoxCollider): void {
     if (_otherCollider.node.getComponent(cc.RigidBody).type === cc.RigidBodyType.Dynamic) {
+      if (this.aux === 0) {
+        this.getComponent(cc.Animation).play('bridge_block_platform');
+      }
       this.isInContact = true;
-      this.getComponent(cc.Animation).play('bridge_block_platform');
+      this.getComponent(cc.Animation).resume();
     }
   }
 
   public onEndContact(_contact: Event, _selfCollider: cc.BoxCollider, _otherCollider: cc.BoxCollider): void {
     this.isInContact = false;
+    this.getComponent(cc.Animation).pause();
   }
 
   public update(dt: number): void {
