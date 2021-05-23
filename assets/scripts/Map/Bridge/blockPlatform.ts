2016986b3753aabc760399eb9845ef234250e49d
LOFyz <leonardo_farias2010@hotmail.com>
2021-05-22 21:26:16 -0300

    Feat: Bridge platform blocks script to fall alfer contact

---
+++ assets/scripts/Map/Bridge/blockPlatform.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class blockPlatform extends cc.Component {
+  private aux = 0;
+
+  private isInContact = false;
+
+  public onBeginContact(_contact: Event, _selfCollider: cc.BoxCollider, _otherCollider: cc.BoxCollider): void {
+    if (_otherCollider.node.getComponent(cc.RigidBody).type === cc.RigidBodyType.Dynamic) {
+      this.isInContact = true;
+    }
+  }
+
+  public onEndContact(_contact: Event, _selfCollider: cc.BoxCollider, _otherCollider: cc.BoxCollider): void {
+    this.isInContact = false;
+  }
+
+  public update(dt: number): void {
+    if (this.isInContact === true && this.aux > 3) {
+      this.aux = 0;
+      this.node.getComponent(cc.RigidBody).type = cc.RigidBodyType.Dynamic;
+      this.scheduleOnce(() => {
+        this.node.destroy();
+      }, 2);
+    } else if (this.isInContact === true) {
+      this.aux += dt;
+    }
+  }
+}
