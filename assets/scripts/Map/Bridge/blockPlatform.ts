0205a80f40f0d4ed0bc31fd9115588d1363d40fe
LOFyz <leonardo_farias2010@hotmail.com>
2021-05-25 19:20:45 -0300

    Fix: Functions of block platform optmized.

--- assets/scripts/Map/Bridge/blockPlatform.ts
+++ assets/scripts/Map/Bridge/blockPlatform.ts
@@ -2,36 +2,16 @@ const { ccclass } = cc._decorator;
 
 @ccclass
 export default class BlockPlatform extends cc.Component {
-  private aux = 0;
-
-  private isInContact = false;
-
   public onBeginContact(_otherCollider: cc.BoxCollider): void {
     if (_otherCollider.node.getComponent(cc.RigidBody).type === cc.RigidBodyType.Dynamic) {
-      if (this.aux === 0) {
-        this.getComponent(cc.Animation).play('bridge_block_platform');
-      }
-      if (this.aux !== 0) {
-        this.getComponent(cc.Animation).resume();
-      }
-      this.isInContact = true;
+      this.getComponent(cc.Animation).play('bridge_block_platform');
     }
   }
 
-  public onEndContact(): void {
-    this.isInContact = false;
-    this.getComponent(cc.Animation).pause();
-  }
-
-  public update(dt: number): void {
-    if (this.isInContact === true && this.aux > 3) {
-      this.aux = 0;
-      this.node.getComponent(cc.RigidBody).type = cc.RigidBodyType.Dynamic;
-      this.scheduleOnce(() => {
-        this.node.destroy();
-      }, 2);
-    } else if (this.isInContact === true) {
-      this.aux += dt;
-    }
+  public onAnimationEnd(): void {
+    this.node.getComponent(cc.RigidBody).type = cc.RigidBodyType.Dynamic;
+    this.scheduleOnce(() => {
+      this.node.destroy();
+    }, 2);
   }
 }
