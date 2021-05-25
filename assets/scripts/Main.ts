e18e259936311b78d20ae44c61cb8e674ea6d3f0
LOFyz <leonardo_farias2010@hotmail.com>
2021-05-25 19:26:25 -0300

    Feat: I enabled physics and collision in the scene.

---
+++ assets/scripts/Main.ts
@@ -0,0 +1,17 @@
+const { ccclass } = cc._decorator;
+
+@ccclass
+export default class Main extends cc.Component {
+  private physicsManager: cc.PhysicsManager;
+
+  private collisionManager: cc.CollisionManager;
+
+  public onLoad(): void {
+    this.physicsManager = cc.director.getPhysicsManager();
+    this.physicsManager.enabled = true;
+    this.physicsManager.gravity = cc.v2(0, -2000);
+
+    this.collisionManager = cc.director.getCollisionManager();
+    this.collisionManager.enabled = true;
+  }
+}
