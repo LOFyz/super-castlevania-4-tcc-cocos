689ca7abb3967e539bc5bf84e9dd2d1c22f1b363
LOFyz <leonardo_farias2010@hotmail.com>
2021-05-28 19:37:01 -0300

    Fix: controller corrected and error that the player doesn't stop, fixed.

--- assets/scripts/Actor/Main_Character/MainCharacter.ts
+++ assets/scripts/Actor/Main_Character/MainCharacter.ts
@@ -1,6 +1,8 @@
 import Actor from '../Actor';
 import DIRECTION from '../DIRECTION';
 
+const { ccclass } = cc._decorator;
+@ccclass
 export default class MainCharacter extends Actor {
   public move(direction: DIRECTION): void {
     this.direction = direction;
@@ -12,6 +14,7 @@ export default class MainCharacter extends Actor {
       (this.direction < 0 && this.rigidBody.linearVelocity.x > -this.maxSpeed)
     ) {
       this.rigidBody.applyForceToCenter(cc.v2(this.direction * this.walkForce, 0), true);
+      this.direction = 0;
     }
   }
 }
--- assets/scripts/Controller/main_character/MainCharacterController.ts
+++ assets/scripts/Controller/main_character/MainCharacterController.ts
@@ -1,3 +1,5 @@
+/* eslint-disable dot-notation */
+/* eslint-disable default-case */
 import DIRECTION from '../../Actor/DIRECTION';
 import MainCharacter from '../../Actor/Main_Character/MainCharacter';
 import KeyboardController from '../KeyboardController';
@@ -6,24 +8,38 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class MainCharacterController extends KeyboardController {
-  @property
-  private mainCharacter: MainCharacter = null;
+  @property(MainCharacter)
+  public mainCharacter: MainCharacter = null;
 
   private keysPressed: Array<boolean> = [];
 
-  public onKeyDown(e: cc.Event.EventKeyboard): void {
-    this.keysPressed[cc.macro.KEY[e.keyCode]] = true;
+  public onKeyDown(event: cc.Event.EventKeyboard): void {
+    switch (event.keyCode) {
+      case cc.macro.KEY.a || cc.macro.KEY.left:
+        this.keysPressed['LEFT'] = true;
+        break;
+      case cc.macro.KEY.d || cc.macro.KEY.right:
+        this.keysPressed['RIGHT'] = true;
+        break;
+    }
   }
 
-  public onKeyUp(e: cc.Event.EventKeyboard): void {
-    delete this.keysPressed[cc.macro.KEY[e.keyCode]];
+  public onKeyUp(event: cc.Event.EventKeyboard): void {
+    switch (event.keyCode) {
+      case cc.macro.KEY.a || cc.macro.KEY.left:
+        delete this.keysPressed['LEFT'];
+        break;
+      case cc.macro.KEY.d || cc.macro.KEY.right:
+        delete this.keysPressed['RIGHT'];
+        break;
+    }
   }
 
   public update(): void {
-    if (cc.macro.KEY.a || cc.macro.KEY.left in this.keysPressed) {
+    if ('LEFT' in this.keysPressed) {
       this.mainCharacter.move(DIRECTION.LEFT);
     }
-    if (cc.macro.KEY.d || cc.macro.KEY.right in this.keysPressed) {
+    if ('RIGHT' in this.keysPressed) {
       this.mainCharacter.move(DIRECTION.RIGHT);
     }
   }
