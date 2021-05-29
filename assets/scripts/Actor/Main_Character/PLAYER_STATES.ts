dc85f97a4b83efa0c18c8421241184cc6ccc5bdd
LOFyz <leonardo_farias2010@hotmail.com>
2021-05-29 20:46:06 -0300

    Feat: player state enum

---
+++ assets/scripts/Actor/Main_Character/PLAYER_STATES.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-shadow */
+enum PLAYER_STATES {
+  CrouchWalking = 'Crouch_walking',
+  Death = 'Death',
+  Downstairs = 'Downstairs',
+  Ducking = 'Ducking',
+  Hurt = 'Hurt',
+  Idle = 'Idle',
+  Jumping = 'Jumping',
+  Subweapon = 'Subweapon',
+  Tarzan = 'Tarzan',
+  Upstairs = 'Upstairs',
+  Walking = 'Walking',
+}
+export default PLAYER_STATES;
