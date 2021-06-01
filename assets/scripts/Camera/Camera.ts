00b6646e02b8e1bf3b7c405f0a152e1bd9960c28
LOFyz <leonardo_farias2010@hotmail.com>
2021-06-01 19:35:56 -0300

    Feat: limits to camera

--- assets/scripts/Camera/Camera.ts
+++ assets/scripts/Camera/Camera.ts
@@ -5,6 +5,11 @@ export default class Camera extends cc.Component {
   @property(cc.Node)
   public player: cc.Node = null;
 
+  @property(cc.Node)
+  public map: cc.Node = null;
+
+  public limits: Array<number> = [];
+
   public _targetPosition: number;
 
   public get targetPosition(): number {
@@ -13,11 +18,13 @@ export default class Camera extends cc.Component {
 
   public set targetPosition(value: number) {
     this._targetPosition = value;
-    this.node.x = cc.misc.lerp(this.node.x, value, 0.2);
+    this.node.x = cc.misc.clampf(cc.misc.lerp(this.node.x, value, 0.2), this.limits[0], this.limits[1]);
   }
 
   public start(): void {
     this.node.x = this.player.x;
+    this.limits[0] = this.node.parent.convertToNodeSpaceAR(this.map.getPosition()).x;
+    this.limits[1] = this.map.width * this.map.scaleX - this.node.parent.width;
   }
 
   public update(): void {
