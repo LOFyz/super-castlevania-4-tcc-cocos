c8b939706232409f78ebaa77c6fb658b5e685d2d
LOFyz <leonardo_farias2010@hotmail.com>
2021-06-01 18:58:34 -0300

    Feat: camera method to follow the player

--- assets/scripts/Camera/Camera.ts
+++ assets/scripts/Camera/Camera.ts
@@ -1,4 +1,28 @@
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class Camera extends cc.Component {}
+export default class Camera extends cc.Component {
+  @property(cc.Node)
+  public player: cc.Node = null;
+
+  public _targetPosition: number;
+
+  public get targetPosition(): number {
+    return this._targetPosition;
+  }
+
+  public set targetPosition(value: number) {
+    this._targetPosition = value;
+    this.node.x = cc.misc.lerp(this.node.x, value, 0.2);
+  }
+
+  public start(): void {
+    this.node.x = this.player.x;
+  }
+
+  public update(): void {
+    if (this.targetPosition !== this.player.x) {
+      this.targetPosition = this.player.x;
+    }
+  }
+}
