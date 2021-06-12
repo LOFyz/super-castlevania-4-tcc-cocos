ad5be771009e5be8fdf74f39171089fd2d8d4cb6
LOFyz <leonardo_farias2010@hotmail.com>
2021-06-12 12:16:10 -0300

    Feat: methods to interact with player in contact

--- assets/scripts/Map/Stair.ts
+++ assets/scripts/Map/Stair.ts
@@ -1,3 +1,5 @@
+import MainCharacter from '../Actor/Main_Character/MainCharacter';
+
 /* eslint-disable no-param-reassign */
 const { ccclass } = cc._decorator;
 
@@ -5,14 +7,36 @@ const { ccclass } = cc._decorator;
 export default class Stair extends cc.Component {
   public steps = [];
 
+  public contactDisabled = true;
+
+  public player: MainCharacter = null;
+
+  public orientationX: number;
+
   public start(): void {
     for (let i = 0; i < this.node.parent.childrenCount; i += 1) {
       this.steps[i] = this.node.parent.getChildByName(`${i}`);
     }
+    if (this.node.scaleX > 0) {
+      this.orientationX = this.node.x + this.node.width;
+    } else {
+      this.orientationX = this.node.x;
+    }
   }
 
   public onBeginContact(contact: cc.PhysicsContact, selfCollider: cc.Collider, otherCollider: cc.Collider): void {
+    contact.disabled = this.contactDisabled;
+    if (!this.contactDisabled) {
+      if (otherCollider.getComponent(MainCharacter)) {
+        this.player = otherCollider.getComponent(MainCharacter);
+      }
+      this.player.isInAStair = true;
+    }
+  }
+
+  public onEndContact(contact: cc.PhysicsContact, selfCollider: cc.Collider, otherCollider: cc.Collider): void {
     contact.disabled = true;
-    console.log('a');
+    this.player.isInAStair = false;
+    this.player = null;
   }
 }
